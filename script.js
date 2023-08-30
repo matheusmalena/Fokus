669fca63de54af7bde61164bd96d0e6682f72128
Matheus Malena <matheusmalena28@gmail.com>
2023-08-30 13:26:23 -0300

    ajuste

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
 
-focoBt.addEventListener( 'click', () => {
+focoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco')
 })
 
