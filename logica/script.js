24bc1d01aab6966e26c8b5673555821a92d5cdd0
Matheus Malena <matheusmalena28@gmail.com>
2024-02-09 15:42:54 -0300

    commit

--- logica/script.js
+++ logica/script.js
@@ -8,7 +8,7 @@ const botoes = document.querySelectorAll('.app__card-button')
 const startPauseBt = document.querySelector('#start-pause')
 const musicaFocoInput = document.querySelector('#alternar-musica')
 const iniciarOuPausarBt = document.querySelector('#start-pause span')
-const iniciarOuPausarBtIcone = document.querySelector(".app__card-primary-butto-icon") 
+const iniciarOuPausarBtIcone = document.querySelector(".app__card-primary-button-icon") 
 const tempoNaTela = document.querySelector('#timer')
 
 const musica = new Audio('./sons/luna-rise-part-one.mp3')
