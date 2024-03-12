a2a3c9702a3c950c3a8d433a5ed7b8075876707e
Matheus Malena <matheus@tew.one>
2024-03-12 17:54:15 -0300

    update

--- logica/script.js
+++ logica/script.js
@@ -8,15 +8,16 @@ const botoes = document.querySelectorAll('.app__card-button')
 const startPauseBt = document.querySelector('#start-pause')
 const musicaFocoInput = document.querySelector('#alternar-musica')
 const iniciarOuPausarBt = document.querySelector('#start-pause span')
-const iniciarOuPausarBtIcone = document.querySelector(".app__card-primary-button-icon") 
+const iniciarOuPausarBtIcone = document.querySelector(".app__card-primary-butto-icon") 
 const tempoNaTela = document.querySelector('#timer')
+const btnFechar = document.querySelector('.close');
 
-const musica = new Audio('./sons/luna-rise-part-one.mp3')
-const audioPlay = new Audio('./sons/play.wav');
-const audioPausa = new Audio('./sons/pause.mp3');
+const musica = new Audio('/sons/luna-rise-part-one.mp3')
+const audioPlay = new Audio('/sons/play.wav');
+const audioPausa = new Audio('/sons/pause.mp3');
 const audioTempoFinalizado = new Audio('./sons/beep.mp3')
 
-let tempoDecorridoEmSegundos = 1500
+let tempoDecorridoEmSegundos = 10
 let intervaloId = null
 
 musica.loop = true
@@ -30,7 +31,7 @@ musicaFocoInput.addEventListener('change', () => {
 })
 
 focoBt.addEventListener('click', () => {
-    tempoDecorridoEmSegundos = 1500;
+    tempoDecorridoEmSegundos = 10
     alterarContexto('foco')
     focoBt.classList.add('active')
 })
@@ -47,7 +48,6 @@ longoBt.addEventListener('click', () => {
     longoBt.classList.add('active')
 })
 
-
 function alterarContexto(contexto) {
     mostrarTempo()
     botoes.forEach(function (contexto){
@@ -60,39 +60,62 @@ function alterarContexto(contexto) {
             titulo.innerHTML = `
             Otimize sua produtividade,<br>
                 <strong class="app__title-strong">mergulhe no que importa.</strong>
-            `;
+            `
             break;
         case "descanso-curto":
             titulo.innerHTML = `
             Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>
-            `;
+            ` 
             break;
         case "descanso-longo":
             titulo.innerHTML = `
             Hora de voltar à superfície.<strong class="app__title-strong"> Faça uma pausa longa.</strong>
-            `;
-            break; 
+            `
         default:
             break;
     }
 }
 
-
-const contagemRegressiva = () => {
-    if(tempoDecorridoEmSegundos <= 0){
-        audioTempoFinalizado.play()
-        alert('Tempo finalizado!')
-        const focoAtivo = html.getAttribute('data-contexto') == 'foco'
-        if (focoAtivo) {
-            const evento = new CustomEvent('FocoFinalizado')
-            document.dispatchEvent(evento)
-        }
-        zerar()
-        return
-    }
-    tempoDecorridoEmSegundos -= 1
-    mostrarTempo()
+// Função para abrir o modal
+function openModal() {
+  document.getElementById("modal").style.display = "block";
 }
+
+// Função para fechar o modal
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
+
+// Event listener para fechar o modal clicando no botão "X"
+document.querySelector(".close").addEventListener("click", closeModal);
+
+// Event listener para fechar o modal clicando fora dele
+window.addEventListener("click", function(event) {
+  if (event.target == document.getElementById("modal")) {
+    closeModal();
+  }
+});
+
+// Função para chamar o modal quando o tempo acabar
+function contagemRegressiva() {
+  if (tempoDecorridoEmSegundos <= 0) {
+    audioTempoFinalizado.play();
+    btnFechar.addEventListener('click', () => {
+        audioTempoFinalizado.pause();
+    });
+    openModal();
+    const focoAtivo = html.getAttribute('data-contexto') == 'foco';
+    if (focoAtivo) {
+      const evento = new CustomEvent('FocoFinalizado');
+      document.dispatchEvent(evento);
+    }
+    zerar();
+    return;
+  }
+  tempoDecorridoEmSegundos -= 1;
+  mostrarTempo();
+}
+
 
 startPauseBt.addEventListener('click', iniciarOuPausar)
 
